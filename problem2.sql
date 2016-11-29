ac4d789687263b8495d2c1bd53228392325a4403
ashe-sparcs <wotjd1102@gmail.com>
2016-11-30 00:15:48 +0900

    problem2 unlooped. select into is used instead.

--- problem2.sql
+++ problem2.sql
@@ -4,12 +4,9 @@ CREATE OR REPLACE TRIGGER CheckNumProduct
 BEFORE INSERT ON product
   FOR EACH ROW
   DECLARE
-    product_num NUMBER := 0;
+    product_num NUMBER;
   BEGIN
-    FOR product_cursor IN (SELECT * FROM product WHERE maker=:NEW.maker)
-    LOOP
-      product_num := product_num + 1;
-    END LOOP;
+    SELECT count(model) INTO product_num FROM product WHERE maker=:NEW.maker;
     IF product_num > 9 THEN
       raise_application_error (-20000, 'Every maker can have at most 10 products.');
     END IF;
