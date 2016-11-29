1e2ec5f41a2501e4017e519c3a64d36562803cec
ashe-sparcs <ashe@sparcs.org>
2016-11-29 13:15:01 +0900

    problem 4 made efficient.

--- problem4.sql
+++ problem4.sql
@@ -3,14 +3,10 @@
 CREATE OR REPLACE FUNCTION findCenterPrice
 RETURN NUMBER
 IS
-  v_sum_min NUMBER := 0;
+  v_sum_min NUMBER := -1;
   v_sum_temp NUMBER;
   v_center_price NUMBER := 0;
 BEGIN
-  FOR pc_cursor IN (SELECT price FROM pc)
-  LOOP
-    v_sum_min := v_sum_min + pc_cursor.price;
-  END LOOP;
   FOR pc_cursor IN (SELECT price FROM pc)
   LOOP
     v_sum_temp := 0;
@@ -18,7 +14,7 @@ BEGIN
     LOOP
       v_sum_temp := v_sum_temp + abs(pc_cursor.price - pc_cursor2.price);
     END LOOP;
-    if v_sum_temp < v_sum_min THEN
+    if v_sum_temp < v_sum_min OR v_sum_min = -1 THEN
       v_sum_min := v_sum_temp;
       v_center_price := pc_cursor.price;
     END IF;
